ed46cc2493097a383d7125ab860532a8417dee9e
AmmonjHorton <hor22017@byui.edu>
2025-05-20 17:20:13 -0600

    mostly finished coolpics

--- cool_pics/coolpics.js
+++ cool_pics/coolpics.js
@@ -1,7 +1,26 @@
-const menuButton = document.querySelector(".menu-button");
-function toggleMenu() {
-  const menu = document.querySelector(".menu");
-  menu.classList.toggle("hide");
-}
+const menuButton = document.getElementById('menu-button');
+const menuLinks = document.getElementById('menu-links');
 
-menuButton.addEventListener("click", toggleMenu);
+menuButton.addEventListener('click', () => {
+    menuLinks.classList.toggle('show');
+});
+
+
+
+const images = document.querySelectorAll('.gallery img'); // Only gallery images
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    const dialog = document.createElement('dialog');
+    dialog.innerHTML = `
+      <img src="${image.src}" alt="${image.alt}">
+      <button class="close-viewer">X</button>
+    `;
+    document.body.appendChild(dialog);
+    dialog.showModal();
+
+    dialog.querySelector('.close-viewer').addEventListener('click', () => {
+      dialog.close();
+      dialog.remove();
+    });
+  });
+});
